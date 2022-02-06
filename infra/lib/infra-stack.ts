8f9c1582e7b6f0db25175dcc8961fccd77a1f21e
sun <sun@codebrick.co>
2022-02-07 00:06:24 +0900

    make templates

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,10 +1,8 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core'
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3'
 
-export class InfraStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class InfraStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const bucket = new Bucket(this, 'testBucket', {
@@ -16,5 +14,10 @@ export class InfraStack extends Stack {
     // const queue = new sqs.Queue(this, 'InfraQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new cdk.CfnOutput(this, 'testBucketNameExport', {
+      value: bucket.bucketName,
+      exportName: 'testBucketName',
+    })
   }
 }
